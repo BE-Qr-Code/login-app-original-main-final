0a39e915790e5c149dbf4ce9c1ec24182b193566
Komal Lonkar <lonkarkomal572@gmail.com>
2020-12-05 19:28:57 +0530

    initial commit

--- app/src/main/java/com/example/loginapp/LoginFragment.java
+++ app/src/main/java/com/example/loginapp/LoginFragment.java
@@ -6,8 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -15,15 +13,13 @@ public class LoginFragment extends Fragment {
 
 
     public LoginFragment() {
-        // Required empty public constructor
     }
 
-
-    @Override
+   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
+                            Bundle savedInstanceState) {
+       // Inflate the layout for this fragment
+       return inflater.inflate(R.layout.fragment_login, container, false);
+   }
 
 }
