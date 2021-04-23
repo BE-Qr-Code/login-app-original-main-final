d7e78cd7acacaf4cbba3e694e9d42a0a899da6df
apurvapatil03 <apoorvapatil03@gmail.com>
2021-04-23 21:29:24 +0530

    updated layout

--- app/src/main/java/com/example/loginapp/HomeScreen.java
+++ app/src/main/java/com/example/loginapp/HomeScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,12 +19,18 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* To remove the TOP BAR from the app */
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        /*  */
         setContentView(R.layout.activity_home_screen);
 
         moodleId = findViewById(R.id.moodleId);
         scanQR_btn = findViewById(R.id.scanQR_button);
 
-        moodleId.setText(SharedPref.getInstance(HomeScreen.this).LoggedInUser());
+        moodleId.setText("Hello" + "\t" + SharedPref.getInstance(HomeScreen.this).LoggedInUser());
 
         scanQR_btn.setOnClickListener(new View.OnClickListener() {
             @Override
